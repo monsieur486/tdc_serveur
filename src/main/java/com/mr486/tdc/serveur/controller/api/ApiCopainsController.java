d35ffdd4831cdf687f6f982ee943dc024b0541e6
monsieur486 <monsieur486@gmail.com>
2022-03-22 18:44:44 +0100

    First commit.

--- src/main/java/com/mr486/tdc/serveur/controller/api/ApiCopainsController.java
+++ src/main/java/com/mr486/tdc/serveur/controller/api/ApiCopainsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "http://127.0.0.1:8080", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class ApiCopainsController {
